b65337ba3be4099e753a5d06890be791e1923d31
IranSS <iranildosousa1234@gmail.com>
2023-09-13 17:06:11 -0300

    Exception

---
+++ src/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Throwable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException() {
+		System.out.print("Erro: ");
+	}
+}
